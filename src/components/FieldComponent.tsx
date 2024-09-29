d9d243d752c67f8ce765bafb99e6f6220891bcd1
Danijel <danijel.jakovac@gmail.com>
2024-09-29 15:11:50 +0200

    make component and optional param

--- src/components/FieldComponent.tsx
+++ src/components/FieldComponent.tsx
@@ -3,14 +3,14 @@ import { FunctionComponent, useEffect } from "react";
 import { TFormField } from "synergy-form-generator/types";
 
 type Props = {
-	component: FunctionComponent;
+	component?: FunctionComponent;
 	field: TFormField;
 };
 
 const FieldComponent = observer(({ component, field }: Props) => {
 	const dependencies = [field.error, field.isValid, field.value];
 	useEffect(() => {}, dependencies);
-	const Component = component;
+	const Component = component || field.component;
 	return <Component />;
 });
 
